28a31f3277b1ff35919da584b50a0ade6e81a23d
Daniel <dan.m.netto@gmail.com>
2022-06-28 12:03:50 -0300

    atualização projeto
    - Node
    - Ajustes pastas anteriores

--- exercicios-web/lista-exercicios/exercicio01.js
+++
@@ -1,5 +0,0 @@
-function calculadora (numero1, numero2) {
-	console.log (numero1+numero2 , numero1-numero2, numero1*numero2, numero1/numero2)
-}
-
-calculadora (12,3)
--- exercicios-web/lista-exercicios/exercicio03.js
+++
@@ -1,6 +0,0 @@
-function Expoente (base, expoente) {
-	resultado = base**expoente
-		return (resultado)
-}
-
-console.log(Expoente(3,2))
